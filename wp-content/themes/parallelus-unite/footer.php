5dc485ebcd0a867dd75f378e59f1908fa92e5cc4
Ty Richards <me@tyrichards.com>
2015-05-13 00:54:40 -0500

    test push 2

--- wp-content/themes/parallelus-unite/footer.php
+++ wp-content/themes/parallelus-unite/footer.php
@@ -9,8 +9,8 @@
 				
 					<!-- Column 1 -->
 					<div class="one-third">
-						<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Left')) : ?>
-						<?php endif; ?>
+						<?php // if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Left')) : ?>
+						<?php // endif; ?>
 					</div>
 
 					<!-- Column 2 -->
